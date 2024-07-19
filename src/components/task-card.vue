e877b857726d3f60f89ad0748706c9be71f56e41
Francis Antonio <francis.antonio0927@gmail.com>
2024-07-19 20:51:11 +0800

    link for edit button

--- src/components/task-card.vue
+++ src/components/task-card.vue
@@ -15,17 +15,18 @@
             location.reload()
         }catch(error){
             console.log(error)
-        }
-       
+        }  
     }
 </script>
 
 <template>
     <div class="relative backdrop-blur-sm bg-white/80 p-5 rounded-xl shadow-sm text-violet-950/90 font-medium">
         <div class="flex gap-1 absolute right-0 -top-4">
-            <div :class="`${color} opacity-80 hover:opacity-100 duration-150 rounded-full p-2 w-max cursor-pointer`">
-                <Pen :size="20" color="white"/>
-            </div>
+            <a :href="`?id=${id}`">
+                <div :class="`${color} opacity-80 hover:opacity-100 duration-150 rounded-full p-2 w-max cursor-pointer`">
+                    <Pen :size="20" color="white"/>
+                </div>
+            </a>
             <div class="bg-red-500 hover:bg-red-600 duration-150 rounded-full p-2 w-max cursor-pointer">
                 <Trash2 :size="20" color="white" @click="taskDelete()"/>
             </div>
