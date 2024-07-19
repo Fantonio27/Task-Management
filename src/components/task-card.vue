490d81e2735bc60d5b83225cce47264257c5a5d9
Francis Antonio <francis.antonio0927@gmail.com>
2024-07-19 16:19:43 +0800

    changing font color

--- src/components/task-card.vue
+++ src/components/task-card.vue
@@ -5,7 +5,7 @@
 </script>
 
 <template>
-    <div class="relative backdrop-blur-sm bg-white/80 p-5 rounded-xl shadow-sm text-violet-950/80 font-medium">
+    <div class="relative backdrop-blur-sm bg-white/80 p-5 rounded-xl shadow-sm text-violet-950/90 font-medium">
         <div class="flex gap-1 absolute right-0 -top-4">
             <div :class="`${color} opacity-80 hover:opacity-100 duration-150 rounded-full p-2 w-max cursor-pointer`">
                 <Pen :size="20" color="white"/>
